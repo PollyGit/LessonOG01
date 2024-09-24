f7b79a5561a78843e0b424e5ab1f6f21c5d50d0f
PollyGit <63475486+PollyGit@users.noreply.github.com>
2024-09-24 22:50:47 +0300

    Create OB06_hw_Class_Heroes_Game.py
    OB06_hw_Class_Heroes_Game.py

---
+++ OB06_hw_Class_Heroes_Game.py
@@ -0,0 +1,69 @@
+import random
+from random import randint, choice
+from abc import ABC, abstractmethod
+
+class Hero:
+    def __init__(self, name, health=100, power=20):
+        self.health = health
+        self.power = power
+        self.name = name
+
+    def attack(self,other):
+        if self.is_alive():
+            self.power = randint(20, 40)
+            if other.health >= self.power:
+                other.health -= self.power
+                return (f'{self.name} атаковал {other.name} и нанес {self.power} едениц урона. '
+                        f'У {other.name} осталось {other.health} хп')
+            else:
+                other.health = 0
+                return (f'{self.name} атаковал {other.name} и нанес {self.power} едениц урона. '
+                    f'У {other.name} не осталось хп, он отдыхает. {self.name} Победил!')
+        else:
+            return (f'{self.name} не может атаковать {other.name}, он отдыхает')
+
+
+    def is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+
+    def who_wins(self, other):
+        if self.is_alive and not other.is_alive:
+             print(f'{self.name} Победил')
+        elif other.is_alive and not self.is_alive:
+            print(f'{other.name} Победил')
+        else:
+            print('Битва продолжается до победного!')
+
+
+#На случай, если нужно будет добавлять для классовв различные методы
+class Player(Hero):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Computer(Hero):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+#Класс Игры
+class Game:
+    def start(cls):
+        p3 = input('Введите имя игрока: ')
+        p1 = Player(p3)
+        p2 = Computer('Компьютер')
+        i = 0
+        # p3 = random.choice([p1, p2])
+        while p1.is_alive() and p2.is_alive():
+            print(f' Раунд {i}.')
+            i += 1
+            print(p1.attack(p2))
+            temp = p1
+            p1 = p2
+            p2 = temp
+
+
+game = Game()
+game.start()
